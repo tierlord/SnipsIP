07c9346ce3a13f6dbae82c0e71945a08ae831ef1
Joffrey <>
2019-05-07 14:32:37 +0200

    Update action-ip.py

--- action-ip.py
+++ action-ip.py
@@ -3,15 +3,28 @@
 import os
 from hermes_python.hermes import Hermes
 
+def split_ip (ip):
+    ip_arr = ip.split('.')
+
+    msg = ""
+
+    for part in ip_arr:
+        for i in range (len(part)):
+            msg += part[i] + " "
+        msg += "punkt "
+    
+    return msg
+            
+
 def get_ip (hermes, message):
     msg = ""
     try:
         ipv4 = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
-        msg = "Meine Adresse lautet: " + ipv4
+        msg = "Meine Adresse lautet: " + split_ip(ipv4)
     except:
         try:
             ipv4 = os.popen('ip addr show wlan0').read().split("inet ")[1].split("/")[0]
-            msg = "Meine Adresse lautet: " + ipv4
+            msg = "Meine WLAN Adresse lautet: " + split_ip(ipv4)
         except:
             msg = "Tut mir leid, das kann ich dir gerade selbst nicht sagen."
 
